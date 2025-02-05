826d9afba92f2d948ba9effc7b65035462d3737b
sergouarg <sergou@gmail.com>
2025-02-05 16:11:33 -0300

    Subir proyecto

---
+++ app.js
@@ -0,0 +1,39 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    
+    amigos.push(nombre);
+    input.value = "";
+    actualizarLista();
+}
+
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    amigos.forEach(amigo => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    
+    document.getElementById("resultado").innerHTML = `<li>Amigo Secreto: <strong>${amigoSorteado}</strong></li>`;
+}
